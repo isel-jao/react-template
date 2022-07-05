a8f0bfb4089ea96e27cb35ed95043124f51f8df6
isel-jao <iseljao89@gmail.com>
2022-07-05 14:38:46 +0100

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 /* eslint-disable no-unused-vars */
-import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+const Home = () => <div> Home </div>;
+const About = () => <div> About </div>;
+
+const RouterView = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+};
+
+const Nav = () => {
+  return (
+    <div className="w-full flex jutify-center gap-2">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </div>
+  );
+};
 
 function App() {
-  return <div className="App flex-center">App</div>;
+  return (
+    <div className="App ">
+      <div className="h-screen  dark:bg-sky-700 dark:text-slate-200 bg-slate-100 text-slate-800">
+        <Nav />
+        <RouterView />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./sass/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 reportWebVitals();
